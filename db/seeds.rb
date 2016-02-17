c8d255e2273ce773b151158715ed0101395e4132
Patcharin Sakulkoo <psakulkoo@gmail.com>
2016-02-17 10:05:34 -0800

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,47 @@ Video.delete_all
 
 
 Video.create(
-  embed_url:            "",
+  embed_url:            "Uq7ZrUBlcQA",
   title:                "Give Me Everything",
   album:                "Planet Pit",
   featured_artists:     "Ne-Yo, Afrojack, Nayer",
   released_on:          Date.parse("6-5-2011")
+)
+
+Video.delete_all
+​
+Video.create(
+  embed_url:        "EPo5wWmKEaI",
+  title:            "Give Me Everything",
+  album:            "Planet Pit",
+  featured_artists: "Ne-Yo, Afrojack, Nayer",
+  released_on:      Date.parse("6-5-2011"),
+  tags:             "#dale"
+)
+​
+Video.create(
+  embed_url:        "bTXJQ5ql5Fw",
+  title:            "Time of Our Lives",
+  album:            "Globalization",
+  featured_artists: "Ne-Yo",
+  released_on:      Date.parse("25-12-2011"),
+  tags:             "#dale"
+)
+​
+Video.create(
+  embed_url:        "SmM0653YvXU",
+  title:            "Rain Over Me",
+  album:            "Planet Pit",
+  featured_artists: "Marc Anthony",
+  released_on:      Date.parse("22-6-2011"),
+  tags:             "#dale #marcanthony"
+)
+​
+Video.create(
+  embed_url:        "hHUbLv4ThOo",
+  title:            "Timber",
+  album:            "Meltdown",
+  featured_artists: "Ke$ha",
+  released_on:      Date.parse("25-11-2013"),
+  tags:             "#countrygold"
 )
