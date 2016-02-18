12e1f2b5d3673d88627b5528c072ebd8b27693f1
Patcharin Sakulkoo <psakulkoo@gmail.com>
2016-02-17 16:28:45 -0800

    adding add button with limitations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  root "videos#index"
+
   get "/videos" => "videos#index"
+
+  get "/videos/new" => "videos#new"
+
+  post "/videos" => "videos#create"
+
 end
 
 
