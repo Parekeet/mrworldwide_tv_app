c5fd808ff996424862b2d64827c71c29e933c677
Patcharin Sakulkoo <psakulkoo@gmail.com>
2016-02-18 09:09:28 -0800

    adding edit, delete, and show to mr. world wide

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,6 +4,10 @@ class VideosController < ApplicationController
     @videos = Video.all
   end
 
+  def show
+    @video = Video.find(params[:id])
+  end
+
   def new
     @video = Video.new
   end
@@ -18,8 +22,28 @@ class VideosController < ApplicationController
     end
   end
 
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+  @video = Video.find(params[:id])
+
+  if @video.update_attributes(params.require(:video).permit(:title, :featured_artist, :released_on, :embed_url))
+    redirect_to videos_path
+  else
+    render :edit
+  end
+end
+
   def video_params
     params.require(:video).permit(:title, :embed_url, :released_on, :featured_artists)
   end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to videos_path
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
 
   post "/videos" => "videos#create"
 
+  get "/videos/:id" => "videos#show", as: :video
+
+  get "/videos/:id/edit" => "videos#edit", as: :edit_video
+
+  patch "/videos/:id" => "videos#update"
+
+  delete "/videos/:id" => "videos#destroy", as: :del_video
+
+
 end
 
 
